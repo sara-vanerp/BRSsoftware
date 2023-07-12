ec3ac51847acfbece48746c1a6c4b4ccf4ec5233
Sara van Erp <sara.vanerp@gmail.com>
2023-07-12 13:43:34 +0200

    update shrinkage visualization for revision 1

--- code/visualization_shrinkage.R
+++ code/visualization_shrinkage.R
@@ -9,9 +9,12 @@ x <- rnorm(n, mean = md, sd = sdd)
 
 # prior
 m0 <- 0
-sd0 <- 0.05
+sd01 <- 0.01
+sd02 <- 0.05
+sd03 <- 0.15
 
 # posterior
+sd0 <- sd03 # change per prior
 post.mean <- ((n/(sdd^2) + 1/(sd0^2))^-1)*(n/(sdd^2) * mean(x) + 1/(sd0^2)*m0)
 post.sd <- sqrt((n/(sdd^2) + 1/(sd0^2))^-1)
 
@@ -25,11 +28,11 @@ g <- ggplot(data = data.frame(x = c(-0.5, 0.5)), aes(x)) +
   theme_bw(base_size = 25) + 
   theme(axis.text.y = element_blank(), 
         axis.ticks.y = element_blank(), 
-        legend.position = "bottom",
+        legend.position = "none",
         legend.key.size = unit(1.5, 'cm'))
 
 # save
-png("./results/shrinkage_prior.png", width = 800, height = 800)
-g
+png("./results/shrinkage_prior3.png", width = 800, height = 800)
+g + ggtitle(paste0("Prior standard deviation ", sd0))
 dev.off()
-g
+
